92b560fbb994dcfe5c21f8a2bd254291b9dfa69d
Robin <robin.singh.1991@gmail.com>
2021-06-21 04:14:00 +0000

    Fixes

---
+++ pages/feed.tsx
@@ -0,0 +1,46 @@
+import type { GetStaticPropsContext } from 'next'
+import { Heart } from '@components/icons'
+import { Layout } from '@components/common'
+import { Text, Container } from '@components/ui'
+import { defaultPageProps } from '@lib/defaults'
+import { getConfig } from '@framework/api'
+import { useCustomer } from '@framework/customer'
+import { WishlistCard } from '@components/wishlist'
+import useWishlist from '@framework/wishlist/use-wishlist'
+import getAllPages from '@framework/common/get-all-pages'
+import { HomeFeed } from '../dripp-web/src/App'
+
+export async function getStaticProps({
+  preview,
+  locale,
+}: GetStaticPropsContext) {
+  // Disabling page if Feature is not available
+  if (!process.env.COMMERCE_WISHLIST_ENABLED) {
+    return {
+      notFound: true,
+    }
+  }
+
+  const config = getConfig({ locale })
+  const { pages } = await getAllPages({ config, preview })
+  return {
+    props: {
+      pages,
+      ...defaultPageProps,
+    },
+  }
+}
+
+export default function Feed() {
+  const { data: customer } = useCustomer()
+  // @ts-ignore Shopify - Fix this types
+  const { data, isLoading, isEmpty } = useWishlist({ includeProducts: true })
+
+  return (
+    <Container>
+      <HomeFeed />
+    </Container>
+  )
+}
+
+Feed.Layout = Layout
---
+++ pages/post/[id].tsx
@@ -0,0 +1,30 @@
+import type { GetStaticPropsContext } from 'next'
+import { Heart } from '@components/icons'
+import { Layout } from '@components/common'
+import { Text, Container } from '@components/ui'
+import { defaultPageProps } from '@lib/defaults'
+import { getConfig } from '@framework/api'
+import { useCustomer } from '@framework/customer'
+import { WishlistCard } from '@components/wishlist'
+import useWishlist from '@framework/wishlist/use-wishlist'
+import getAllPages from '@framework/common/get-all-pages'
+import { InfluencerPostDetails } from '../../dripp-web/src/App'
+import { useRouter } from 'next/router'
+
+export default function Post() {
+  const { data: customer } = useCustomer()
+  const { data, isLoading, isEmpty } = useWishlist({ includeProducts: true })
+  const router = useRouter()
+
+  console.log(router)
+
+  return (
+    <Container>
+      <InfluencerPostDetails query={router.query} />
+    </Container>
+  )
+}
+Post.getInitialProps = (options) => {
+  return {}
+}
+Post.Layout = Layout
